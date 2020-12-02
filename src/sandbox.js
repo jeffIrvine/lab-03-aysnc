03bfcce91452cbd4ba97d39463f941650d08fb01
Your NamejeffIrvine <jirvine1212@gmail.com>
2020-12-02 14:35:10 -0800

    initial commit

---
+++ src/sandbox.js
@@ -0,0 +1,25 @@
+// const fsPromises = require('fs').promises;
+
+// fsPromises.readFile('./README.md', 'utf-8')
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
+
+// fsPromises.writeFile('./README.md', 'hello there!')
+//     .then(data => {
+//         console.log('done!');
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
+
+// fsPromises.readFile('./README.md', 'utf-8')
+//     .then(data => {fsPromises.writeFile('./README2.md', data)
+//     })
+
+//     .catch(err => {
+//         console.log(err);
+//     })
